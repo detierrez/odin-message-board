d2a577fea84cfd636c85e92536d2a9749d3dcb86
detierrez <detierrez@gmail.com>
2026-02-24 12:53:53 -0300

    Add input validation

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,4 +1,23 @@
+const { body, validationResult } = require("express-validator");
+
 const db = require("../db/queries");
+
+const validateMessage = [
+  body("username")
+    .trim()
+    .isAlphanumeric()
+    .withMessage(`must contain only letters, numbers or '_'`)
+    .isLength({ min: 1 })
+    .withMessage(`is obligatory`)
+    .isLength({ max: 64 })
+    .withMessage(`must contain no more than 64 characters`),
+  body("text")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage(`is obligatory`)
+    .isLength({ max: 200 })
+    .withMessage(`must contain no more than 200 characters`),
+];
 
 module.exports.getMessages = async (req, res) => {
   const messages = await db.getMessages();
@@ -9,11 +28,29 @@ module.exports.getNewMessage = (req, res) => {
   res.render("boilerplate", { view: "form" });
 };
 
-module.exports.postNewMessage = async (req, res) => {
-  const { username, text } = req.body;
-  await db.createMessage(new Date(), username, text);
-  res.redirect("/");
-};
+module.exports.postNewMessage = [
+  validateMessage,
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) return next();
+
+    const { username, text } = req.body;
+    return res
+      .status(400)
+      .render("boilerplate", {
+        view: "form",
+        errors: errors.array(),
+        username,
+        text,
+      });
+  },
+  async (req, res) => {
+    const { username, text } = req.body;
+    await db.createMessage(new Date(), username, text);
+    res.redirect("/");
+  },
+];
 
 module.exports.getMessage = async (req, res) => {
   const { id } = req.params;
